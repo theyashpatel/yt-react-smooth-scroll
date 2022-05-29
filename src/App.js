2a824126c0b6853c02b2be77d0f76b7d40973cc0
theyashpatel <yashpatel26@gmail.com>
2022-05-29 21:09:46 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link } from "react-scroll";
 
 function App() {
+  const menuItems = [
+    {
+      id: 1,
+      title: "home",
+    },
+    {
+      id: 2,
+      title: "portfolio",
+    },
+    {
+      id: 3,
+      title: "contact",
+    },
+    {
+      id: 4,
+      title: "about",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <h2>Coffee Shop</h2>
+
+          <ul>
+            {menuItems.map((menu) => (
+              <li>
+                <Link
+                  to={menu.title}
+                  smooth={true}
+                  offset={-430}
+                  duration={500}
+                >
+                  {menu.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </header>
+
+      <main>
+        {menuItems.map((menu) => (
+          <div className="content">
+            <h1 className="content-header" id={menu.title}>
+              {menu.title}
+            </h1>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
